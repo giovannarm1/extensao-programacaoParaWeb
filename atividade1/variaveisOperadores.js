06eab90014baae3198d55528f7a029774cec6ed7
Giovanna Rodrigues <141952631+giovannarm1@users.noreply.github.com>
2024-03-14 09:59:56 -0300

    Add files via upload

---
+++ atividade1/variaveisOperadores.js
@@ -0,0 +1,20 @@
+let num_A = 5;
+let num_B = 3;
+
+// Adição
+let rAdicao = num_A + num_B;
+console.log(`Resultado da adição entre ${num_A} e ${num_B} é ${rAdicao}`);
+
+//Subtração
+let rSubtracao = num_A - num_B;
+console.log(`Resultado da subtração entre ${num_A} e ${num_B} é: ${rSubtracao}`);
+
+//Divisão
+let rDivisao = num_A / num_B;
+console.log(`Resultado da divisão entre ${num_A} e ${num_B} é: ${rDivisao}`);
+
+//Multiplicação
+let rMultiplicacao = num_A * num_B;
+console.log(`Resultado da multiplicação entre ${num_A} e ${num_B} é: ${rMultiplicacao}`);
+
+
